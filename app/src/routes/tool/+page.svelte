6d8701877ebf6c4c0393523e2069d6de5bc36d1c
Lukas Schulte <lukas.schulte@uni-passau.de>
2023-05-16 09:32:50 +0000

    adds navigation buttons

--- app/src/routes/tool/+page.svelte
+++ app/src/routes/tool/+page.svelte
@@ -4,7 +4,23 @@
 
 <!-- BODY -->
 
+<div class="wrapper">
+	<a href="/tool/label" role="button">Labeling</a>
+	<a href="/tool/harmonize" role="button">Harmonizing</a>
+</div>
+
 <!-- STYLE -->
 
 <style lang="scss">
+	.wrapper {
+		position: absolute;
+		bottom: 45%;
+		left: 25%;
+		width: 50%;
+
+		a {
+			width: 100%;
+			margin-bottom: 15px;
+		}
+	}
 </style>
