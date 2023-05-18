4f7871f1890863837a9c33a93b192dfa0ef469aa
Lukas Schulte <l.schulte@mailbox.org>
2023-05-18 14:42:29 +0000

    adds progress statistic

--- app/src/routes/tool/+page.svelte
+++ app/src/routes/tool/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
 	import '@picocss/pico';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	console.log(data);
 </script>
 
 <!-- BODY -->
 
 <div class="wrapper">
+	<table role="grid">
+		<thead>
+			<tr>
+				<th scope="col">Category</th>
+				<th scope="col">Progress</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Verified issues related to privacy</td>
+				<td>{data.numberOfPricacyIssues}</td>
+			</tr>
+			{#each data.userProgress as progress}
+				<tr>
+					<td>Issues coded ({progress[0]})</td>
+					<td>{progress[1]} / {data.numberOfIssues}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+
 	<a href="/tool/label" role="button">Labeling</a>
 	<a href="/tool/harmonize" role="button">Harmonizing</a>
 </div>
@@ -14,7 +40,7 @@
 <style lang="scss">
 	.wrapper {
 		position: absolute;
-		bottom: 45%;
+		bottom: 30%;
 		left: 25%;
 		width: 50%;
 
--- app/src/routes/tool/variables.ts
+++ app/src/routes/tool/variables.ts
@@ -5,3 +5,14 @@ export const dbConfig = {
     password: import.meta.env.VITE_DB_PW,
     database: import.meta.env.VITE_DB_DB
 }
+
+import type { Client } from 'ts-postgres';
+
+export async function checkUserToken(token: string | undefined, client: Client) {
+    if (!token) {
+        return null
+    }
+
+    const result = await client.query('SELECT * FROM users WHERE token = $1', [token]).one()
+    return result?.get('name') ?? null
+}
