df8fe6867e46abd5081a32babedd0c7523f000f3
Lukas Schulte <l.schulte@mailbox.org>
2023-07-23 11:10:25 +0000

    changes sql query to do the opposite thing

--- app/src/routes/tool/+page.server.ts
+++ app/src/routes/tool/+page.server.ts
@@ -27,7 +27,7 @@ export const load = (async (event) => {
 		const index = Number.parseInt(row.get('index')) + 1;
 		const query = new Query(
 			`SELECT COUNT(*) FROM gh_issues ` +
-				`WHERE is_privacy_related = false AND consent_interaction_rater_${index} IS NOT NULL ` +
+				`WHERE is_privacy_related = true AND consent_interaction_rater_${index} IS NOT NULL ` +
 				`AND resolution_rater_${index} IS NOT NULL AND privacy_issue_rater_${index} IS NOT NULL`
 		);
 
