c7859540c23ae23f0fa29ae6a85f0cf169f54638
Lukas Schulte <l.schulte@mailbox.org>
2023-07-23 11:08:10 +0000

    fixes rater progress in case issues are re-classified

--- app/src/routes/tool/+page.server.ts
+++ app/src/routes/tool/+page.server.ts
@@ -1,39 +1,46 @@
 import { Client, Query } from 'ts-postgres';
-import { checkUserToken, dbConfig } from "./variables";
-import type { PageServerLoad } from "./$types"
+import { checkUserToken, dbConfig } from './variables';
+import type { PageServerLoad } from './$types';
 
 const client = new Client(dbConfig);
 await client.connect();
 
 export const load = (async (event) => {
-    const userToken = event.cookies.get('userToken')
-    const user = await checkUserToken(userToken, client)
-    if (user == null) {
-        throw Error("Unauthorized")
-    }
+	const userToken = event.cookies.get('userToken');
+	const user = await checkUserToken(userToken, client);
+	if (user == null) {
+		throw Error('Unauthorized');
+	}
 
+	const resultIssuesRelatedToPrivacy = await client
+		.query('SELECT COUNT(*) FROM gh_issues WHERE is_privacy_related = $1', [true])
+		.one();
+	const resultIssuesNotRelatedToPrivacy = await client
+		.query('SELECT COUNT(*) FROM gh_issues WHERE is_privacy_related = $1', [false])
+		.one();
 
-    const resultIssuesRelatedToPrivacy = await client.query('SELECT COUNT(*) FROM gh_issues WHERE is_privacy_related = $1', [true]).one()
-    const resultIssuesNotRelatedToPrivacy = await client.query('SELECT COUNT(*) FROM gh_issues WHERE is_privacy_related = $1', [false]).one()
+	const resultUsers = await client.query('SELECT index, name FROM users LIMIT 2');
+	const userProgress: [string, string][] = [];
+	for (const row of resultUsers) {
+		const username = row.get('name');
 
-    const resultUsers = await client.query('SELECT index, name FROM users LIMIT 2')
-    const userProgress: [string, string][] = []
-    for (const row of resultUsers) {
-        const username = row.get("name")
+		const index = Number.parseInt(row.get('index')) + 1;
+		const query = new Query(
+			`SELECT COUNT(*) FROM gh_issues ` +
+				`WHERE is_privacy_related = false AND consent_interaction_rater_${index} IS NOT NULL ` +
+				`AND resolution_rater_${index} IS NOT NULL AND privacy_issue_rater_${index} IS NOT NULL`
+		);
 
-        const index = Number.parseInt(row.get("index")) + 1
-        const query = new Query(`SELECT COUNT(*) FROM gh_issues WHERE consent_interaction_rater_${index} IS NOT NULL AND resolution_rater_${index} IS NOT NULL AND privacy_issue_rater_${index} IS NOT NULL`)
+		const progress = await client.execute(query).one();
+		userProgress.push([username, progress.get('count')]);
+	}
 
-        const progress = await client.execute(query).one()
-        userProgress.push([username, progress.get('count')])
-    }
+	const resultNumberOfIssues = await client.query('SELECT COUNT(*) FROM gh_issues').one();
 
-    const resultNumberOfIssues = await client.query('SELECT COUNT(*) FROM gh_issues').one()
-
-    return {
-        numberOfPrivacyIssues: resultIssuesRelatedToPrivacy.get('count'),
-        numberOfNonPrivacyIssues: resultIssuesNotRelatedToPrivacy.get('count'),
-        userProgress,
-        numberOfIssues: resultNumberOfIssues.get('count')
-    }
-}) satisfies PageServerLoad
+	return {
+		numberOfPrivacyIssues: resultIssuesRelatedToPrivacy.get('count'),
+		numberOfNonPrivacyIssues: resultIssuesNotRelatedToPrivacy.get('count'),
+		userProgress,
+		numberOfIssues: resultNumberOfIssues.get('count')
+	};
+}) satisfies PageServerLoad;
